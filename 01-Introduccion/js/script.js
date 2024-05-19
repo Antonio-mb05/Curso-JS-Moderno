aec060b8da72df4d071b3d4d4a51ae9a203fdfed
Antonio Marquez Barrios <158839900+Antonio-mb05@users.noreply.github.com>
2024-05-19 19:40:24 +0200

    Ejemplo Uso JS
    Ejemplo del uso que podemos dar con tan solo dos o tres líneas en JS

--- 01-Introduccion/js/script.js
+++ 01-Introduccion/js/script.js
@@ -1,2 +1,8 @@
-alert("Hola Mundo con Alert");
-console.log("Hola Mundo con Consola");
+// Preguntamos nombre y guardamos en variable
+const nombre = prompt("Cual es tu nombre?");
+
+//Muestra la variable en consola
+console.log("Hola "+nombre);
+
+// Vacía el .Contenido del div y muestra el nombre en pantalla
+document.querySelector(".contenido").innerHTML = `${nombre} está aprendiendo JavaScript `;
