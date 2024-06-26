34e02ce83695e6664caef84918f2ddd08c60207e
Antonio Marquez Barrios <158839900+Antonio-mb05@users.noreply.github.com>
2024-06-26 13:33:16 +0200

    06 Booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const autentificado = true;
+
+if(autentificado){
+    console.log ('Si puedes ver netflix')
+} else {
+    console.log('No puedes acceder')
+}
+
+// Operador ternario
+console.log (autentificado ? 'Si está autentificado' : 'No está autentificado');
